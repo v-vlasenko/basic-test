199ff3d38eeffaf3f9488ba618e76ee225f1f918
Vladyslav Vlasenko <fl4shqq@gmail.com>
2022-10-13 15:15:01 +0300

    Update main.tf

--- pet-count/main.tf
+++ pet-count/main.tf
@@ -1,10 +1,19 @@
-resource "random_pet" "pet" {
-  count = 9
+# resource "random_pet" "pet" {
+#   count = 9
+#   keepers = {
+#     # Generate a new pet name each time
+#     name = "Vladyslav"
+#   }
+# }
+
+resource "random_pet" "test" {
   keepers = {
-    # Generate a new pet name each time
-    name = "Vladyslav"
+    keeper = var.user_var
   }
 }
+
+variable "user_var" {}
+
 output "very_long" {
   value = "Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
 }
