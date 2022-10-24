5217a0034d675f31d48554f590619001413663c8
Vladyslav Vlasenko <fl4shqq@gmail.com>
2022-10-24 19:18:53 +0300

    Update main.tf

--- pet-count/main.tf
+++ pet-count/main.tf
@@ -1,96 +1,96 @@
 resource "random_pet" "pet" {
-  count = 9
+  count = 1
   keepers = {
     # Generate a new pet name each time
     name = "Vlad"
   }
 }
 
-output "very_long" {
-  value = "Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
-}
-
-output "senc_out" {
-  value = "secret data"
-  description = "my sensitive output"
-  sensitive = true
-}
-
-output "pets" {
-  value = random_pet.pet[*].id
-}
-
-output "pet_keeper" {
-  value = random_pet.pet[0].keepers.name
-}
-
-variable "string" {
-  type = string
-  default = "stringValue"
-}
-
-variable "boolean" {
-  type = bool
-  default = false
-}
-
-variable "number" {
-  type = number
-  default = 12
-}
-
-variable "float" {
-  type = number
-  default = 15.55
-}
-
-# variable "null" {
-#   type = null
-#   default = null
+# output "very_long" {
+#   value = "Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
 # }
 
-variable "list" {
-  type = list
-  default = ["322", true, 15.22]
-}
+# output "senc_out" {
+#   value = "secret data"
+#   description = "my sensitive output"
+#   sensitive = true
+# }
 
-variable "map" {
-  type = map
-  default = {
-    key1 = "value 1"
-    key2 = "value 2"
-  }
-}
+# output "pets" {
+#   value = random_pet.pet[*].id
+# }
 
-variable "object" {
-  type = object ({
-    ob_1 = bool
-    ob_2 = string
-  })
-  default = {
-    ob_1 = true
-    ob_2 = "stringObjectValue"
-  }
-}
+# output "pet_keeper" {
+#   value = random_pet.pet[0].keepers.name
+# }
 
-resource "null_resource" "test" {
-  triggers = {
-    trigger = "1"
-  }
-}
+# variable "string" {
+#   type = string
+#   default = "stringValue"
+# }
 
-output "var_map_key_1" {
-  value = var.map.key1
-}
+# variable "boolean" {
+#   type = bool
+#   default = false
+# }
 
-output "var_map_key_2" {
-  value = var.map.key2
-}
+# variable "number" {
+#   type = number
+#   default = 12
+# }
 
-output "var_map" {
-  value = var.map
-}
+# variable "float" {
+#   type = number
+#   default = 15.55
+# }
 
-output "CAPITALIZED_OUTPUT" {
-  value = "BIG LETTERS ARE GOOD"
-}
+# # variable "null" {
+# #   type = null
+# #   default = null
+# # }
+
+# variable "list" {
+#   type = list
+#   default = ["322", true, 15.22]
+# }
+
+# variable "map" {
+#   type = map
+#   default = {
+#     key1 = "value 1"
+#     key2 = "value 2"
+#   }
+# }
+
+# variable "object" {
+#   type = object ({
+#     ob_1 = bool
+#     ob_2 = string
+#   })
+#   default = {
+#     ob_1 = true
+#     ob_2 = "stringObjectValue"
+#   }
+# }
+
+# resource "null_resource" "test" {
+#   triggers = {
+#     trigger = "1"
+#   }
+# }
+
+# output "var_map_key_1" {
+#   value = var.map.key1
+# }
+
+# output "var_map_key_2" {
+#   value = var.map.key2
+# }
+
+# output "var_map" {
+#   value = var.map
+# }
+
+# output "CAPITALIZED_OUTPUT" {
+#   value = "BIG LETTERS ARE GOOD"
+# }
